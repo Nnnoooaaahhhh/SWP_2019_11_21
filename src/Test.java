cd19d4ac6e8d5dcf07dc74f17f3075c7fa3772ad
Nnnoooaaahhh <noahgrobbel1919@gmail.com>
2019-11-21 13:08:56 +0100

    first

---
+++ src/Test.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Test {
+
+	public static void main(String[] args){
+				
+		int [] arr = new int [10];
+		int z = 0;
+		String lol = "Hi";
+		
+		
+		try {
+		FileReader fr = new FileReader("C:\\Users\\noahg\\Desktop\\teeest.txt");
+		} 
+		catch (FileNotFoundException a) {
+			System.out.println("\n"+"DU KEK DE DATEI GIBSCH NET");
+			a.printStackTrace();
+		} 
+			  
+		try {
+			for (int i = 0; i < arr.length+1; i++) {
+				arr[i] = i;
+			}
+		}
+		catch (IndexOutOfBoundsException b) {
+			System.out.println("\n"+"DEN INDEX GIBS BA DEM ARRAY NET DU KEK");
+			b.printStackTrace();
+		}
+		
+		try {
+			z = Integer.parseInt(lol);
+		}
+		catch (NumberFormatException c) {
+			System.out.println("\n"+"DU KUSCH KOANE BUCHSTOBN IN AN INT SCHREIBN");
+			c.printStackTrace();
+		}
+	}
+}
